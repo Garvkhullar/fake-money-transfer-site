ea551d5882052f32a00324244b03260a5bfa1e79
Garvkhullar <garv.khullar@gmail.com>
2025-06-21 23:59:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,8 +71,8 @@ app.post('/transfer', async (req, res) => {
   res.redirect('/');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
 });
+
 
